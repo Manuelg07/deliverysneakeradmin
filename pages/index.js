db10e2a7f791e005b0e6768c2c42828137309e20
Manuel Godoy <manuelgodoyvzla@gmail.com>
2023-07-06 17:05:07 +0200

    Commit admin

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,23 @@
 import react from "react"
 import { useSession, signIn, signOut } from "next-auth/react"
+import Nav from "@/components/nav";
 
 export default function Home() {
   const { data: session } = useSession();
   if (!session){
     return (
-      <div className="bg-gray-900 w-screen h-screen flex items-center">
+      <div className="bg-white w-screen h-screen flex items-center">
       <div className="text-center w-full">
-        <button onClick={() => signIn("google")} className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
+        <button onClick={() => signIn("google")} className="bg-[#FAAF5A] text-black p-2 px-4 rounded-lg">Login with Google</button>
       </div>
       </div>)
   }
 
   return (
-    <div>logged in {session.user.email}</div>
+    <div className="bg-black-900 min-h-screen flex">
+    <Nav/>
+      <div className="bg-white flex-grow mt-2 mr-2 rounded-lg p-4 mb-2 text-black">logged in {session.user.email}</div>
+    </div>
+   
   );
 }
